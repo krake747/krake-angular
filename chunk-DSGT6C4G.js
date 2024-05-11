5cb1ccdbc6b4027a72d7a70cff61f1f1c5ec27c0
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-05-11 22:27:07 +0200

    Auto-generated commit

---
+++ chunk-DSGT6C4G.js
@@ -0,0 +1,1 @@
+import"./chunk-ECLQBL7H.js";var e=[{path:"",providers:[],children:[{path:"",loadComponent:()=>import("./chunk-OOHKKEGY.js").then(o=>o.HomeComponent)}]}];export{e as default};
--- chunk-F23XVBTU.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ECLQBL7H.js";var e=[{path:"",providers:[],children:[{path:"",loadComponent:()=>import("./chunk-EYEURRWK.js").then(o=>o.HomeComponent)}]}];export{e as default};
